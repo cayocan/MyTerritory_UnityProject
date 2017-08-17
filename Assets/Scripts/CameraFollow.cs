1cbcd8a0965a7cf8dae33c3a2427cf466ef4e173
Cayo Aguiar <cayocan@hotmail.com>
2017-08-16 22:45:08 -0300

    CameraFollow implementado e testado

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Vector3 offset;
+
+    private Transform playerTransform;
+
+    private void OnEnable()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        this.transform.position = playerTransform.position + offset;
+        this.transform.LookAt(playerTransform);
+	}
+}
